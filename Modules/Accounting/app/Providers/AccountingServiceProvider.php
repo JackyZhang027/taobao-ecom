330ed25c97e197a43239ccfbdcddd424d1f37b35
Jacky Zhang <jacky.chang123456789@gmail.com>
2026-03-10 20:43:22 +0700

    fix issue in optimze

--- Modules/Accounting/app/Providers/AccountingServiceProvider.php
+++ Modules/Accounting/app/Providers/AccountingServiceProvider.php
@@ -102,12 +102,13 @@ class AccountingServiceProvider extends ServiceProvider
      */
     public function registerViews(): void
     {
-        $viewPath = resource_path('views/modules/'.$this->nameLower);
         $sourcePath = module_path($this->name, 'resources/views');
 
-        $this->publishes([$sourcePath => $viewPath], ['views', $this->nameLower.'-module-views']);
-
-        $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->nameLower);
+        if (is_dir($sourcePath)) {
+            $viewPath = resource_path('views/modules/'.$this->nameLower);
+            $this->publishes([$sourcePath => $viewPath], ['views', $this->nameLower.'-module-views']);
+            $this->loadViewsFrom(array_merge($this->getPublishableViewPaths(), [$sourcePath]), $this->nameLower);
+        }
 
         $componentNamespace = $this->module_namespace($this->name, $this->app_path(config('modules.paths.generator.component-class.path')));
         Blade::componentNamespace($componentNamespace, $this->nameLower);
