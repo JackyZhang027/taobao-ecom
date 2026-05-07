bdc783165a47df7b1f0c6eb82ef9823288aa5abd
Jacky Zhang <jacky.chang123456789@gmail.com>
2026-05-07 21:43:41 +0700

    fix: correct import statement for Transition component in profile settings page

--- resources/js/pages/settings/profile.tsx
+++ resources/js/pages/settings/profile.tsx
@@ -1,4 +1,4 @@
-npm import { Transition } from '@headlessui/react';
+import { Transition } from '@headlessui/react';
 import { Form, Head, Link, usePage } from '@inertiajs/react';
 import ProfileController from '@/actions/App/Http/Controllers/Settings/ProfileController';
 import DeleteUser from '@/components/delete-user';
